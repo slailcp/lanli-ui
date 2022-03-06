2af1c32a338db6d8a316ebd31f5c5333157408c1
slailcp <1350033585@qq.com>
2022-03-06 15:45:00 +0800

    替换

--- README.md
+++
@@ -1,9 +0,0 @@
-# lanli-ui
-
-## 安装
-```
-npm i -S lanli-ui
-```
-
-## [文档]（https://slailcp.github.io/lanli-ui/index.html）
-
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-import { Layer, Toast, Icons } from '../packages'
-
-
-createApp(App).use(store).use(router).use(Layer).use(Toast).use(Icons).mount('#app')
