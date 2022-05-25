21934904561b4551440ddec7aa2c76b60e1722a2
shenlanlan <fjsll1102>
2022-05-25 15:52:31 +0800

    scroll

--- packages/index.ts
+++
@@ -1,7 +0,0 @@
-import "./style/base.less"
-
-export * from "./popup"
-export * from "./layer"
-export * from "./toast"
-export * from "./icons"
-export * from "./calendar"
--- packages/utils/types.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentPublicInstance } from 'vue'
-
-export type Interceptor = (
-  ...args: any[]
-) => Promise<boolean> | boolean | undefined | void;
-
-export type ComponentInstance = ComponentPublicInstance<{}, any>;
